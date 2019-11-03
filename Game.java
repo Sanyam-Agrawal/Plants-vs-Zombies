31d2cdfff52460b99462f1b1bd3deabac85b2d40
shubhammitt <shubham18101@iiitd.ac.in>
2019-11-03 16:56:55 +0530

    Added grid

--- Game.java
+++ Game.java
@@ -11,6 +11,8 @@ import javafx.geometry.*;
 
 public class Game extends Scenes
 {
+    private Tile[][] centerTiles = new Tile[5][9];
+    private Pane[] centerRows = new Pane[5];
     public Game(App app,Stage st)
     {
         super(app,st);
@@ -37,13 +39,35 @@ public class Game extends Scenes
         game_menu.setOnAction(game_menu_event);
         gameVBox.getChildren().add(game_menu);
 
-        root.setCenter(gameVBox);
+        root.setLeft(gameVBox);
+        root.setCenter(this.createCenterTiles());
 
         Scene scene=new Scene(root,1100,600);
         scene.getStylesheets().add(Game.class.getResource("game.css").toExternalForm());
         return scene;
     }
-
+    public VBox createCenterTiles(){
+		VBox centerTiles = new VBox(50);
+		centerTiles.setSpacing(55);
+		centerTiles.setMaxWidth(950.0);
+		centerTiles.setMaxHeight(475.0);
+			int centerTilesRowCount = 0;
+			int centerTilesColCount = 0;
+ 			for (int r=0; r<5; r++){
+				Pane row = new Pane();
+				int startingXCoord = 140;
+				for (int c=0; c<9; c++){
+					Tile tile = new Tile(this, startingXCoord, r, c, Color.TRANSPARENT);
+					this.centerTiles[r][c] = tile;
+					tile.setUpCenterTile();
+					row.getChildren().add(tile);
+					startingXCoord += 81;
+				}
+				this.centerRows[r] = row;
+				centerTiles.getChildren().add(row);
+    		}
+		return centerTiles;
+	}
     public static void main(String[] args){
 
     }

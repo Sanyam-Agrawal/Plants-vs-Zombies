da01cb2b0e5a2ab519c7e76afd424d9faee232c5
Sanyam-Agrawal <j11sanyam@gmail.com>
2019-11-29 19:14:38 +0530

    Basic GridObject

--- GridObject.java
+++ GridObject.java
@@ -1,12 +1,31 @@
+import javafx.stage.*;
+import javafx.event.*;
+import javafx.scene.image.*;
+import javafx.scene.control.*;
+import javafx.scene.input.*;
+import javafx.scene.paint.*;
+import javafx.scene.*;
+import javafx.scene.text.*;
+import javafx.scene.layout.*;
+import javafx.geometry.*;
+import javafx.animation.*; 
+import javafx.util.Duration;
+
 class GridObject implements Serializable
 {
-	double xpos, ypos, xdelta, ydelta;
+	private double xdelta, ydelta;
 
-	GridObject (double i1, double i2, double i3, double i4)
+	private VBox vBox;
+
+	GridObject (VBox _vBox, double i1, double i2)
 	{
-		xpos = i1;
-		ypos = i2;
-		xdelta = i3;
-		ydelta = i4;
+		vBox = _vBox;
+		xdelta = i1;
+		ydelta = i2;
 	}
+
+	public VBox getVBox() { return this.vBox; }
+
+	public double getXdelta() { return this.xdelta; }
+	public double getYdelta() { return this.ydelta; }
 }
